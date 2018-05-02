b26a88e7ae247ebff2ad1218636d074e715045d6
George Sheaffer <gsheaf@gmail.com>
2018-05-02 17:45:52 -0400

    Update

---
+++ shotty/shotty.py
@@ -0,0 +1,102 @@
+import boto3
+import click
+
+session = boto3.Session(profile_name='shotty')
+ec2 = session.resource('ec2')
+
+def filter_instances(project):
+    instances = []
+
+    if project:
+        filters = [{'Name':'tag:Project', 'Values':[project]}]
+        instances = ec2.instances.filter(Filters=filters)
+    else:
+        instances = ec2.instances.all()
+
+    return instances
+
+@click.group()
+def instances():
+    """Commands for isntances"""
+
+@instances.command('list')
+@click.option('--project', default=None,
+    help="Only instances for project (tag Project:<name>)")
+def list_instances(project):
+    "List EC2 instances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        tags = { t['Key']: t['Value'] for t in i.tags or [] }
+        print(', '.join((
+        i.id,
+        i.instance_type,
+        i.placement['AvailabilityZone'],
+        i.state['Name'],
+        i.private_ip_address,
+        i.root_device_name,
+        i.public_dns_name,
+        tags.get('Project', '<no project>'))))
+
+    return
+
+@instances.command('snapshot')
+@click.option('--project', default=None,
+    help='Create snapshot of all volumes')
+def create_snapshots(project):
+    "Create snapshot for EC2 isntances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+
+        i.stop()
+        i.wait_until_stopped()
+
+        for v in i.volumes.all():
+            print("Creating snapshot of {0}...".format(v.id))
+            v.create_snapshot(Description="Create by SnapshotAlyzer 300")
+
+        print("Starting {0}...".format(i.id))
+
+        i.start()
+        i.wait_until_running()
+
+    print("Job's done!")
+    
+    return
+
+
+@instances.command('stop')
+@click.option('--project', default=None,
+    help='Only instances for project')
+def stop_instances(project):
+    "Stop EC2 isntances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Stopping {0}...".format(i.id))
+        i.stop()
+
+    return
+
+@instances.command('start')
+@click.option('--project', default=None,
+    help='Only instances for project')
+def start_instances(project):
+    "Starting EC2 isntances"
+
+    instances = filter_instances(project)
+
+    for i in instances:
+        print("Starting {0}...".format(i.id))
+        i.start()
+
+    return
+
+
+if __name__ == '__main__':
+    instances()
